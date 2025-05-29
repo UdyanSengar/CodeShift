7a72c5e78bb37402bcd4a5c40444baef5d39a610
UdyanSengar <udyansengar7@gmail.com>
2025-05-30 01:49:07 +0530

    Setup React + Tailwind CSS initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to CodeShift UI</h1>
+      <p className="text-gray-700 mt-2">React + Tailwind CSS is working!</p>
     </div>
   );
 }
